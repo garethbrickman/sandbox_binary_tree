fac60f9aaa2e9e8831d03ae37e463737f6e2a432
Gareth Brickman <977@holbertonschool.com>
2019-12-03 19:19:22 +0000

    [FIX] Betty comply

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -11,10 +11,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	else
-	{
-		binary_tree_delete(tree->right);
-		binary_tree_delete(tree->left);
-		free(tree);
-	}
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
