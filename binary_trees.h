e3a71521067ea65e2124a6b2e796d48472677d7a
Gareth Brickman <977@holbertonschool.com>
2019-12-05 22:17:31 +0000

    [ADD/FIX] Add 110, include prototype in header

--- binary_trees.h
+++ binary_trees.h
@@ -47,4 +47,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* Advanced */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif
